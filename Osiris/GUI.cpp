ac9ad32b4a78f700cad3d4e2863f749fecdbaf5a
MesuDevastator <administrator@laptismc.xyz>
2022-12-23 12:39:57 +0800

    Fix build date formatting

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -2594,7 +2594,7 @@ void GUI::renderGuiStyle() noexcept
                     ImGui::BeginChild("##Text", ImVec2{ 700, 20 }, false);
                     {
                         ImGui::SetCursorPos(ImVec2{ 2, 2 });
-                        ImGui::Text("Better Osiris Made In https://github.com/MesuDevastator/OsirisAndExtra | Build Date: " __DATE__ __TIME__);
+                        ImGui::Text("Better Osiris Made In https://github.com/MesuDevastator/OsirisAndExtra | Build Date: " __DATE__ " " __TIME__);
                     }
                     ImGui::EndChild();
                 }
